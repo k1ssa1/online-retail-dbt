aa3b41f46d63944ebbfb67da662791a2252e6cbe
k1ssa1 <samikissai.edu@gmail.com>
2026-08-19 19:45:57 +0000

    fix: add is_cancelled field to intermediate stage and fact table

--- models/intermediate/int_online_retail.sql
+++ models/intermediate/int_online_retail.sql
@@ -15,7 +15,11 @@ WITH
       ABS(unit_price) AS unit_price,
       COALESCE(customer_id, 0) AS customer_id,
       COALESCE(country, 'Unknown') AS country,
-      quantity * ABS(unit_price) AS total_cost
+      quantity * ABS(unit_price) AS total_cost,
+      CASE
+        WHEN invoice_id LIKE 'C%' THEN true
+        ELSE false
+      END AS is_cancelled
     FROM
       staged_data
     WHERE
--- models/marts/fact_transaction.sql
+++ models/marts/fact_transaction.sql
@@ -8,7 +8,8 @@ SELECT
     t.invoice_id,
     t.quantity,
     t.unit_price,
-    t.total_cost
+    t.total_cost,
+    t.is_cancelled
     FROM {{ref('int_online_retail')}} t
     INNER JOIN {{ ref('dim_customer') }} c ON c.customer_id = t.customer_id
     INNER JOIN {{ ref('dim_product') }} p ON p.stock_code = t.stock_code

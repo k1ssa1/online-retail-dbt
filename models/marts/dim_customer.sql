1d8073c19bdc2abdefd50c5d0a28b48fd149082d
k1ssa1 <samikissai.edu@gmail.com>
2026-08-25 16:34:10 +0000

    fix: enforce unique customer dimension grain

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -1,29 +1,40 @@
-WITH
-  clean_data AS (
+WITH clean_data AS (
     SELECT
-      *
-    FROM
-      {{ref('int_online_retail') }}
-  ),
-  dim_customer_nsk AS (
+        *
+    FROM {{ ref('int_online_retail') }}
+),
+
+customer_countries AS (
     SELECT
-      DISTINCT customer_id,
-      country
-    FROM
-      clean_data
-  ),
-  dim_customer AS (
+        customer_id,
+        country,
+        COUNT(*) AS country_count
+    FROM clean_data
+    GROUP BY
+        customer_id,
+        country
+),
+
+dim_customer_nsk AS (
     SELECT
-      ROW_NUMBER() OVER (
-        ORDER BY
-          customer_id
-      ) AS customer_key,
-      customer_id,
-      country
-    FROM
-      dim_customer_nsk
-  )
-SELECT
-  *
-FROM
-  dim_customer
+        customer_id,
+        country
+    FROM customer_countries
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY customer_id
+        ORDER BY country_count DESC, country
+    ) = 1
+),
+
+dim_customer AS (
+    SELECT
+        ROW_NUMBER() OVER (
+            ORDER BY customer_id
+        ) AS customer_key,
+        customer_id,
+        country
+    FROM dim_customer_nsk
+)
+
+SELECT *
+FROM dim_customer
